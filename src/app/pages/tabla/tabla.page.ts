f09542a46fd3e4658cddc289762307ef4ce3c086
John Silvestr <johnkerlin52@gmail.com>
2025-02-06 17:00:23 -0400

    feat: creating design page translate

--- src/app/pages/tabla/tabla.page.ts
+++ src/app/pages/tabla/tabla.page.ts
@@ -10,4 +10,18 @@ export class TablaPage implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  numero: number | null = null;
+  tabla: string[] = [];
+
+  generarTabla() {
+    if (this.numero === null) {
+      this.tabla = ['Por favor, ingresa un número válido.'];
+      return;
+    }
+
+    this.tabla = [];
+    for (let i = 1; i <= 13; i++) {
+      this.tabla.push(`${this.numero} x ${i} = ${this.numero * i}`);
+    }
+  }
 }
